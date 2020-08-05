621c21d945ea6dab225c6810525264ae05c2663e
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-05 15:23:13 +0700

    add project description

--- Com.Danliris.Service.Auth.Test/Helper/ValidateServiceTest.cs
+++ Com.Danliris.Service.Auth.Test/Helper/ValidateServiceTest.cs
@@ -1,5 +1,6 @@
-﻿using Com.Danliris.Service.Auth.Lib.Models;
-using Com.Danliris.Service.Auth.Lib.Services.ValidateService;
+﻿using Com.Danliris.Service.Auth.Lib.Services.ValidateService;
+using Com.Danliris.Service.Auth.Lib.Utilities;
+using Com.Danliris.Service.Auth.Lib.ViewModels;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -10,18 +11,23 @@ namespace Com.Danliris.Service.Auth.Test.Helper
 {
     public class ValidateServiceTest
     {
+        
         [Fact]
-        public void Should_Success_Validate()
+        public void Should_Success_Instantiate()
         {
-            var serviceProviderMock = new Mock<IServiceProvider>();
-            ValidateService validate = new ValidateService(serviceProviderMock.Object);
-            Permission permission = new Permission()
+            Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+            ValidateService service = new ValidateService(serviceProviderMock.Object);
+            UnitViewModel stage = new UnitViewModel()
             {
-                Division = "Division"
+                Code = "Code",
+                Division =new DivisionViewModel()
+                {
+                    Name ="Name"
+                },
+                Name ="Name"
             };
-           
-          validate.Validate(permission);
-          
+            service.Validate(stage);
+
         }
     }
 }
