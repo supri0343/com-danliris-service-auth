692a0ef2c0cdf4acb3d8ce0c942569f73cee1b92
ardana96 <ardanagama@gmail.com>
2023-08-29 16:35:18 +0700

    Add Monitoring Account

---
+++ Com.Danliris.Service.Auth.Lib/BusinessLogic/Interfaces/IAccountMonitoringService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Com.Danliris.Service.Auth.Lib.BusinessLogic.Interfaces
+{
+    public interface IAccountMonitoringService
+    {
+        MemoryStream GetExcel(int userId, string menu);
+    }
+}
